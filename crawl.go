35b9e317216877a9ca5e65e639598fb6572c4688
Serb <mkoctuk@gmail.com>
2017-03-31 13:07:49 +0300

    add fetch with proxy

--- crawl.go
+++ crawl.go
@@ -3,7 +3,21 @@ package pool
 import (
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"time"
 )
+
+type IpType struct {
+	Addr        string
+	Port        string
+	Ssl         bool
+	isWork      bool
+	isAnon      bool
+	ProxyChecks int
+	CreateAt    time.Time
+	LastCheck   time.Time
+	Response    time.Duration
+}
 
 func crawl(t *task) {
 	t.result, t.err = fetchBody(t.address)
@@ -28,36 +42,36 @@ func fetchBody(targetURL string) ([]byte, error) {
 	return body, err
 }
 
-// func fetchBodyWProxy(targetURL string, proxy ipType) ([]byte, error) {
-// 	client := &http.Client{
-// 		Timeout: time.Duration(timeout) * time.Second,
-// 	}
-// 	if proxy.Addr != "" {
-// 		var (
-// 			proxyURL *url.URL
-// 			err      error
-// 		)
-// 		if proxy.Ssl {
-// 			proxyURL, err = url.Parse("https://" + proxy.Addr + ":" + proxy.Port)
-// 		} else {
-// 			proxyURL, err = url.Parse("http://" + proxy.Addr + ":" + proxy.Port)
-// 		}
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		client.Transport = &http.Transport{
-// 			Proxy:             http.ProxyURL(proxyURL),
-// 			DisableKeepAlives: true,
-// 		}
-// 	}
-// 	resp, err := client.Get(targetURL)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	body, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer resp.Body.Close()
-// 	return body, err
-// }
+func fetchBodyWProxy(targetURL string, proxy IpType) ([]byte, error) {
+	client := &http.Client{
+	// Timeout: time.Duration(timeout) * time.Second,
+	}
+	if proxy.Addr != "" {
+		var (
+			proxyURL *url.URL
+			err      error
+		)
+		if proxy.Ssl {
+			proxyURL, err = url.Parse("https://" + proxy.Addr + ":" + proxy.Port)
+		} else {
+			proxyURL, err = url.Parse("http://" + proxy.Addr + ":" + proxy.Port)
+		}
+		if err != nil {
+			return nil, err
+		}
+		client.Transport = &http.Transport{
+			Proxy:             http.ProxyURL(proxyURL),
+			DisableKeepAlives: true,
+		}
+	}
+	resp, err := client.Get(targetURL)
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	return body, err
+}
