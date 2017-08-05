1938272b6811ab936b816eb32bd951f565e3d038
Valeriy Kostikov <mkoctuk@gmail.com>
2017-08-05 20:55:52 +0300

    fix timeout

--- worker.go
+++ worker.go
@@ -1,17 +1,18 @@
 package pool
 
-import "time"
-
 func (p *Pool) worker(id int) {
 	for task := range p.workChan {
+		if p.timerIsRunning {
+			p.timer.Stop()
+		}
 		p.dec()
 		task.WorkerID = id
 		crawl(task)
 		p.ResultChan <- *task
 		p.inc()
 		p.endTaskChan <- true
-		if p.timerIsRunning {
-			p.timer = time.NewTimer(p.quitTimeout)
+		if p.timerIsRunning && p.freeWorkers == p.numWorkers {
+			p.timer.Reset(p.quitTimeout)
 		}
 	}
 }
