2ae353600ca2408d09529b9bf18a2baf29227bc2
Valeriy Kostikov <mkoctuk@gmail.com>
2017-08-11 21:44:27 +0300

    remove len

--- list.go
+++ list.go
@@ -1,35 +1,26 @@
 package pool
 
 import (
-	"errors"
 	"sync"
 )
 
-var (
-	// errEmptyTaskList = errors.New("task list is empty")
-	errNilTask = errors.New("task is nil")
-)
-
 type taskList struct {
-	m   sync.Mutex
-	len int
-	val []Task
+	m    sync.Mutex
+	list []Task
 }
 
 func (t *taskList) put(task Task) {
 	t.m.Lock()
-	t.val = append(t.val, task)
-	t.len++
+	t.list = append(t.list, task)
 	t.m.Unlock()
 }
 
 func (t *taskList) get() (Task, bool) {
 	t.m.Lock()
 	var task Task
-	if t.len > 0 {
-		task = t.val[0]
-		t.len--
-		t.val = t.val[1:]
+	if len(t.list) > 0 {
+		task = t.list[0]
+		t.list = t.list[1:]
 		t.m.Unlock()
 		return task, true
 	}
